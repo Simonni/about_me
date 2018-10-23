39c3fb0289799ebf5a31d97ebe19a1308a1f16ee
Simonni <szkiflay@gmal.com>
2018-10-23 15:07:18 -0400

    adding lab-2

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+// WHo Am I
+ let name = prompt('Who am I?')
+console.log('My name is ', name)
+ 
+if(name.toLowerCase() === 'simon'&& name.toUpperCase() === 'SIMON') {
+  alert(name + ' Great! You got it, Welcome')
+ } else if (name==='Simon') {
+   alert('You are closer!')
+ } else{
+  alert('This is not me dud')
+  
+   alert('No Worries, let go to my age?')
+ }
+  
+ // Aboy My Age
+ let age = prompt('Guess my Age?')
+  if (age ===30){
+    alert('WOW, You Got it!')
+  } else {
+    alert('Hmm... Your Guess is not good!')
+  }
+    
+ // About My Hobby 
+  let hobby = prompt('What abot my Hobby?')
+  if (hobby ==='Basketball' || hobby==='Soccer'){
+    alert('Got it!')
+  } else{
+alert('That\'s a good Guess')
+  }
+//Do you Like Me
+  let lastQ = prompt('Do you like Me')
+  if(lastQ.toLowerCase() ==='yes'||lastQ.toUpperCase() ==='YES' ){
+    alert('I LOVE U TOO!')
+  } else{  
+alert ('That\'s not cool')
+  }
+ 
+
+
+  
